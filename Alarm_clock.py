d46cd8e1912c21b2d7fbb40b117227f75374a44f
bruno_gaddini <bruno_gaddini@hotmail.com>
2022-11-30 15:35:15 -0300

    Initial commit

---
+++ Alarm_clock.py
@@ -0,0 +1,16 @@
+## Import Libraries
+import tkinter
+import PIL
+
+from tkinter.ttk import *
+from tkinter import *
+from PIL import Image, ImageTk
+
+## Color Pallete
+
+cr0 = "#f0f3f5" ## Black
+cr1 = "#feffff" ## White
+cr2 = "#d6872d" ## Gold
+cr3 = "#fc766d" ## Red
+cr4 = "#403d3d" ## Letter Color
+cr5 = "#4a88e8" ## Blue
