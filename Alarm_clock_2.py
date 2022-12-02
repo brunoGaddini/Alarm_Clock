9b16c1f6cc186fe75f82994902e8a0768ab20a81
bruno_gaddini <bruno_gaddini@hotmail.com>
2022-12-02 14:44:06 -0300

    Update Alarm_clock_2.py

--- Alarm_clock_2.py
+++ Alarm_clock_2.py
@@ -1,4 +1,4 @@
-## Import Libraries
+# Import Libraries
 import tkinter
 import PIL
 
@@ -6,26 +6,26 @@ from tkinter.ttk import *
 from tkinter import *
 from PIL import Image, ImageTk
 
-## Color Pallete
+# Color Pallete
 
-cr0 = "#f0f3f5" ## Black
-cr1 = "#feffff" ## White
-cr2 = "#d6872d" ## Gold
-cr3 = "#fc766d" ## Red
-cr4 = "#403d3d" ## Letter Color
-cr5 = "#4a88e8" ## Blue
+cr0 = "#f0f3f5"  # Black
+cr1 = "#feffff"  # White
+cr2 = "#d6872d"  # Gold
+cr3 = "#fc766d"  # Red
+cr4 = "#403d3d"  # Letter Color
+cr5 = "#4a88e8"  # Blue
 
-## Creating the Window
+# Creating the Window
 
-window= Tk()
+window = Tk()
 window.title("")
 window.geometry('350x150')
 window.configure(background=cr1)
 window.resizable(width=False, height=False)
 
-#window.mainloop()
+# window.mainloop()
 
-## Splitting the window into two frames
+# Splitting the window into two frames
 
 frame_logo = Frame(window, width=400, height=10, background=cr2)
 frame_logo.grid(row=0, column=0, pady=1, padx=0)
@@ -33,30 +33,35 @@ frame_logo.grid(row=0, column=0, pady=1, padx=0)
 frame_body = Frame(window, width=400, height=290, background=cr1)
 frame_body.grid(row=1, column=0, pady=1, padx=0)
 
-## Configuring the logo frame
+# Configuring the logo frame
 
 l_line = Label(frame_logo, height=1, background=cr2, anchor=NW, font=('Ivy 1'))
 l_line.place(x=0, y=0)
 
-## Configuring the body frame
+# Configuring the body frame
 
 l_line = Label(frame_logo, height=1, background=cr2, anchor=NW, font=('Ivy 1'))
 l_line.place(x=0, y=0)
 
-## Configuring the image frame
+# Configuring the image frame
 
-imagem = Image.open(r'C:\Users\Bruno\Desktop\Python\Depertador\icon1.png')
+imagem = Image.open(r'C:\Users\brunogaddini.intern\Desktop\PYTHON\Projeto_Despertador\icon1.png')
 imagem = imagem.resize((100, 100))
 imagem = ImageTk.PhotoImage(imagem)
 
-l_imagem = Label(frame_body, height=100, image=imagem, compound=LEFT, padx=10, anchor=NW, font=('Ivy 16 bold'), background=cr1, fg=cr3) ## fg = letter color
+l_imagem = Label(frame_body, height=100, image=imagem, compound=LEFT, padx=10, anchor=NW, font=('Ivy 16 bold'), background=cr1, fg=cr3)  # fg = letter color
 l_imagem.place(x=10, y=10)
 
-## Configuring the name frame
+# Configuring the name frame
 
-l_imagem = Label(frame_body, height=100, image=imagem, compound=LEFT, padx=10, anchor=NW, font=('Ivy 16 bold'), background=cr1, fg=cr3) ## fg = letter color
+l_imagem = Label(frame_body, height=100, image=imagem, compound=LEFT, padx=10, anchor=NW, font=('Ivy 16 bold'), background=cr1, fg=cr3)
 l_imagem.place(x=10, y=10)
+
+# Configuring the text frame
+
+l_text = Label(frame_body, text='Alarme', height=1, anchor=NE, font=('Ivy 10 bold'), background=cr1, fg=cr4)  # fg = text color
+l_text.place(x=10, y=10)
 
 window.mainloop()
 
-## Second Video 08:50
+# 8:50
