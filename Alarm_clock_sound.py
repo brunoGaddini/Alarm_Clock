d9ca49f2f903d2eaadffc4606be20c3446d6d021
bruno_gaddini <bruno_gaddini@hotmail.com>
2022-12-04 00:37:02 -0300

    Initial commit

--- Alarm_clock_sound.py
+++ Alarm_clock_sound.py
@@ -1,5 +1,16 @@
 from pygame import mixer
+from tkinter import *
 
-mixer.init # Initializing the mixer
+#cr1 = "#feffff"  # White
+
+window = Tk()
+window.title("")
+window.geometry('350x150')
+#window.configure(background=cr1)
+window.resizable(width=False, height=False)
+
+mixer.init() # Initializing the mixer
 mixer.music.load('sound1.mp3')
 mixer.music.play()
+
+window.mainloop()
