16abe05dd8629cb7a4ce68de19ae6f7700b401e8
bruno_gaddini <bruno_gaddini@hotmail.com>
2022-12-04 17:51:46 -0300

    Initial commit

--- Alarm_clock_sound.py
+++ Alarm_clock_sound.py
@@ -2,6 +2,7 @@ import time
 
 from pygame import mixer
 from datetime import datetime
+from time import sleep
 from tkinter import *
 
 #cr1 = "#feffff"  # White
@@ -12,7 +13,7 @@ window.geometry('350x150')
 #window.configure(background=cr1)
 window.resizable(width=False, height=False)
 
-def play_alarm():
+def ring_alarm():
     mixer.init()  # Initializing the mixer
     mixer.music.load('sound1.mp3')
     mixer.music.play()
@@ -32,5 +33,13 @@ def alarm():
         second = current_time.strftime("%S")
         period = current_time.strftime("%p")
 
+        if str(control) ==(1):
+            if c_period == period:
+                if c_hour == hour:
+                    if c_minutes == minut:
+                        if c_seconds == second:
+                            print("Time to take a break")
+                            ring_alarm()
+        sleep(1)
 
 window.mainloop()
