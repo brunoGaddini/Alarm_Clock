badbdc2b11fe60396cd2a90fe8d90c3e1ca3a159
bruno_gaddini <bruno_gaddini@hotmail.com>
2022-12-04 16:50:20 -0300

    Initial commit

--- Alarm_clock_sound.py
+++ Alarm_clock_sound.py
@@ -1,4 +1,7 @@
+import time
+
 from pygame import mixer
+from datetime import datetime
 from tkinter import *
 
 #cr1 = "#feffff"  # White
@@ -13,5 +16,21 @@ def play_alarm():
     mixer.init()  # Initializing the mixer
     mixer.music.load('sound1.mp3')
     mixer.music.play()
+
+def alarm():
+    while True:
+        control = 1
+        c_hour = "00"
+        c_minutes = "30"
+        c_seconds = "00"
+        c_period = "am".upper()
+
+        current_time = datetime.now()
+
+        hour = current_time.strftime("%I")
+        minut = current_time.strftime("%M")
+        second = current_time.strftime("%S")
+        period = current_time.strftime("%p")
+
 
 window.mainloop()
